418feddf80687c6c6ed8cb33eabfe7afcc0966c1
supertiger1234 <saawalkhn123@googlemail.com>
2019-08-05 20:10:54 +0100

    removed min limit to password

--- policies/UserPolicies.js
+++ policies/UserPolicies.js
@@ -7,9 +7,9 @@ const policies = {
     .not().contains('@').withMessage("username cannot contain @").optional({checkFalsy: true}),
 
     check('tag').isString().withMessage('Invalid Format.').isLength({min: 4, max: 4}).withMessage('Tag must be 4 characters long.').optional({checkFalsy: true}),
-    check('password').isString().withMessage('Invalid Format.').isLength({min: 3}).withMessage("Password must be at least 3 chars long.").optional({checkFalsy: true}),
+    check('password').isString().withMessage('Invalid Format.').optional({checkFalsy: true}),
     check('email').isEmail().withMessage('Invalid email.').optional({checkFalsy: true}),
-    check('new_password').isString().withMessage('Invalid Format.').isLength({min: 3}).withMessage("Password must be at least 3 chars long.").optional({checkFalsy: true}),
+    check('new_password').isString().withMessage('Invalid Format.').isLength({min: 3}).withMessage("New password must be at least 3 chars long.").optional({checkFalsy: true}),
     check('avatar').isString().withMessage('Invalid Format.').optional({checkFalsy: true}),
     policyHandler
   ],
