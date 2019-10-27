df6a3a2ac76f2c3b3c5b15c6d057acdfa363d8b1
supertiger1234 <saawalkhn@googlemail.com>
2019-10-27 14:33:51 +0000

    Only allow alphanumeric characters as the tag.

--- policies/UserPolicies.js
+++ policies/UserPolicies.js
@@ -6,7 +6,7 @@ const policies = {
     check('username').isString().withMessage('Invalid Format.').isLength({min: 3, max: 30}).withMessage("Username must be between 3 - 30 chars long.")
     .not().contains('@').withMessage("username cannot contain @").optional({checkFalsy: true}),
 
-    check('tag').isString().withMessage('Invalid Format.').isLength({min: 4, max: 4}).withMessage('Tag must be 4 characters long.').optional({checkFalsy: true}),
+    check('tag').isString().withMessage('Invalid Format.').isAlphanumeric().withMessage('Tag must contain alphanumeric characters and no spaces.').isLength({min: 4, max: 4}).withMessage('Tag must be 4 characters long.').optional({checkFalsy: true}),
     check('password').isString().withMessage('Invalid Format.').optional({checkFalsy: true}),
     check('email').isEmail().withMessage('Invalid email.').optional({checkFalsy: true}),
     check('new_password').isString().withMessage('Invalid Format.').isLength({min: 3}).withMessage("New password must be at least 3 chars long.").optional({checkFalsy: true}),
