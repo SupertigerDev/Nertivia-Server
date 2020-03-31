dc36d6e1c28a68ed272a74494ed16bb9c49df44e
supertiger1234 <saawalkhn@googlemail.com>
2020-03-31 19:03:01 +0100

    using "UnorderedBulkOp" to send notifications.

--- src/utils/SendMessageNotification.js
+++ src/utils/SendMessageNotification.js
@@ -1,16 +1,21 @@
-
-const Messages = require('./../models/messages');
-const ServerMembers = require('./../models/ServerMembers');
-const Notifications = require('./../models/notifications');
-
+const Messages = require("./../models/messages");
+const ServerMembers = require("./../models/ServerMembers");
+const Notifications = require("./../models/notifications");
 
 // recipient_uniqueID: Only required for DMs.
 // server_id: Only required for server messages.
-async function sendNotification({message, server_id, recipient_uniqueID, channelID, sender}) {
-
+async function sendNotification({
+  message,
+  server_id,
+  recipient_uniqueID,
+  channelID,
+  sender
+}) {
+  const bulk = Notifications.collection.initializeUnorderedBulkOp();
   // If messages are to be sent to dm.
   if (recipient_uniqueID) {
-    await findOneAndUpdate(recipient_uniqueID, sender._id, channelID, message);
+    await addToBulk(bulk, recipient_uniqueID, sender._id, channelID, message);
+    bulk.execute();
     return;
   }
 
@@ -21,44 +26,45 @@ async function sendNotification({message, server_id, recipient_uniqueID, channel
     muted_channels: { $ne: channelID }
   }).populate("member");
 
-  // Convert JSON Array to array of uniqueID 
+  // Convert JSON Array to array of uniqueID
   const members_uniqueID = members
     .map(m => m.member.uniqueID)
     .filter(m => m !== sender.uniqueID);
 
-  let notificationPromises = [];
   for await (const memberUniqueID of members_uniqueID) {
-    notificationPromises.push(findOneAndUpdate(memberUniqueID, sender._id, channelID, message));
+    addToBulk(bulk, memberUniqueID, sender._id, channelID, message)
   }
-  await Promise.all(notificationPromises);
+  bulk.execute();
   return members_uniqueID;
 }
 
-function findOneAndUpdate (recipient_uniqueID, sender_id, channelID, message) {
-
+function addToBulk(bulk, recipient_uniqueID, sender_id, channelID, message) {
   const $set = {
     recipient: recipient_uniqueID,
     channelID,
     type: "MESSAGE_CREATED",
     lastMessageID: message.messageID,
     sender: sender_id
-  }
-  const mentioned = message.mentions && !!message.mentions.find(m => m.uniqueID === recipient_uniqueID);
+  };
+  const mentioned =
+    message.mentions &&
+    !!message.mentions.find(m => m.uniqueID === recipient_uniqueID);
   if (mentioned) {
     $set.mentioned = true;
   }
 
-  return Notifications.findOneAndUpdate({
-    recipient: recipient_uniqueID,
-    channelID
-  },{
-    $set,
-    $inc: {
-      count: 1
-    }
-  },{
-    upsert: true
-  })
+  bulk
+    .find({
+      recipient: recipient_uniqueID,
+      channelID
+    }).upsert()
+    .update(
+      {
+        $set,
+        $inc: { count: 1 }
+      },
+    );
+    return;
 }
 
 module.exports = sendNotification;
