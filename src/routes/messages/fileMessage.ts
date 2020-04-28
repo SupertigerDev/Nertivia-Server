aef8a5b50ad0c7c100ae1a6d6fea1fe9e0d202e2
supertiger1234 <saawalkhn@googlemail.com>
2020-04-28 17:24:37 +0100

    added compression settings

--- src/routes/messages/fileMessage.ts
+++ src/routes/messages/fileMessage.ts
@@ -3,27 +3,37 @@ import busboy from 'connect-busboy';
 import path from 'path';
 import fs from 'fs';
 import * as nertiviaCDN from '../../utils/uploadCDN/nertiviaCDN';
-import  uploadGoogleDrive from '../../utils/uploadCDN/googleDrive';
+import uploadGoogleDrive from '../../utils/uploadCDN/googleDrive';
 import sharp from 'sharp';
-import {Request, Response, NextFunction} from 'express';
+import gm from 'gm';
+const gmInstance = gm.subClass({ imageMagick: true });
+import { Request, Response, NextFunction } from 'express';
 const oauth2Client = require('./../../middlewares/GDriveOauthClient')
 const FlakeId = require('flakeid');
-const flakeId = new FlakeId(); 
+const flakeId = new FlakeId();
 
 export default async (req: Request, res: Response, next: NextFunction) => {
   let cancelRequest = false;
   // if formdata doesnt exist, go to the next function.
   if (!req.headers["content-type"]?.startsWith('multipart/form-data')) return next();
+  // file size limit
+  const filesize = parseInt(req.headers['content-length'] || "0");
+  const file_limit = 50340000; // 50MB
+  if (filesize > file_limit) {
+    return res.status(403).json({message: '50MB file or below.'})
+  }
+
   // start getting file stream;
-  busboy({immediate: true})(req, res, () => {}) // empty function to stop next() from executing.
+  busboy({ immediate: true })(req, res, () => { }) // empty function to stop next() from executing.
 
   let message: string;
   let upload_cdn: number; // 0: google drive 1: nertivia cdn
+  let compress:number = 0; // 0: dont compress 1: compress
 
-  req.busboy.on("error", function(err:any) {
+  req.busboy.on("error", function (err: any) {
     cancelRequest = true;
     req.unpipe(req.busboy);
-    return res.status(403).json({message: 'Something went wrong while trying to upload.'})
+    return res.status(403).json({ message: 'Something went wrong while trying to upload.' })
   })
 
   req.busboy.on('field', (fieldname, val) => {
@@ -40,108 +50,115 @@ export default async (req: Request, res: Response, next: NextFunction) => {
     if (fieldname === 'upload_cdn') {
       upload_cdn = parseInt(val);
     }
+    if (fieldname === 'compress') {
+      compress = parseInt(val) || 0;
+    }
   })
-  req.busboy.on("file", async (fieldname, file, filename, encoding, mimetype ) => {
-    if (fieldname !== 'file'){
+  req.busboy.on("file", async (fieldname, file, filename, encoding, mimetype) => {
+    if (fieldname !== 'file') {
       req.unpipe(req.busboy);
-      return res.status(403).json({message: 'Use field name of "file" to upload files.'})
+      return res.status(403).json({ message: 'Use field name of "file" to upload files.' })
     }
 
-    file.on("error",  function(err) {
+    file.on("error", function (err) {
       if (cancelRequest) return;
       cancelRequest = true;
       req.unpipe(req.busboy);
-      return res.status(403).json({message: 'Something went wrong while trying to upload.'})
+      return res.status(403).json({ message: 'Something went wrong while trying to upload.' })
     })
 
     if (upload_cdn === undefined) {
       req.unpipe(req.busboy);
-      return res.status(403).json({message: 'upload_cdn is missing or ordered incorrectly.'})
+      return res.status(403).json({ message: 'upload_cdn is missing or ordered incorrectly.' })
     }
 
-    // get image dimentions by getting the first buffer.
-    let chunks: Buffer[] = [];
+    // get image dimentions
     let metadata: sharp.Metadata;
-    if (isImage(filename, mimetype )) file.on("data", onData);
-    async function onData(chunk: any) {
-      if (chunks.length === 0) {
-        chunks.push(chunk);
-      }
-      const buffer = Buffer.concat(chunks);
-      if (metadata) return;
-      metadata = await sharp(buffer).metadata();
-      file.removeListener("data", onData);
-    }
+
 
     const fileid = flakeId.gen();
-    const dirPath = path.join(__dirname, "../", "../", "public", "temp", fileid);
+    const dirPath = path.join(__dirname, "../", "../", "public", "temp", filename);
 
     // temporarly store file in server.
     const writeStream = fs.createWriteStream(dirPath);
     file.pipe(writeStream);
 
-    // Upload to Google Drive
-    if (upload_cdn === 0) {
-      oauth2Client(req, res, () => {});
+    writeStream.once("close", async () => {
 
-      writeStream.once("close", async () => {
+      if (isImage(filename, mimetype)) {
+        if (compress) {
+          const compressed = await new Promise((res, rej) => {
+            gmInstance(dirPath)
+              .resize(1920, 1080, ">")
+              .quality(50)
+              .autoOrient()
+              .interlace("Plane")
+              // png files seem to be large :/ dont know how to fix.
+              .write(dirPath, err => {
+                if (err) return rej(err);
+                res(true);
+              })
+          }).catch(() => { res.status(403).json({ message: "Failed to compress image." }) })
+          if (!compressed) return deleteFile(dirPath);
+        }
+        metadata = await sharp(dirPath).metadata();
+      }
+
+
+      // Upload to Google Drive
+      if (upload_cdn === 0) {
+        oauth2Client(req, res, () => { });
         if (cancelRequest) return;
-        const data:any = await uploadGoogleDrive({
+        const data: any = await uploadGoogleDrive({
           file: {
             fileName: filename,
             dirPath: dirPath,
             mimeType: mimetype
           },
           oauth2Client: req.oauth2Client
-        }).catch(_ => {res.status(403).json({message: "Something went wrong while uploading to Google Drive."})})
+        }).catch(_ => { res.status(403).json({ message: "Something went wrong while uploading to Google Drive." }) })
         if (!data) return deleteFile(dirPath);
-        const fileObj: {fileName: string, fileID: string, dimensions?: object} = {
+        const fileObj: { fileName: string, fileID: string, dimensions?: object } = {
           fileName: filename,
           fileID: data.data.id
         };
-        if (metadata){
+        if (metadata) {
           fileObj.dimensions = { width: metadata.width, height: metadata.height };
         }
-        req.uploadFile = {file: fileObj, message}
+        req.uploadFile = { file: fileObj, message }
         next()
         // delete local file after job is done.
         deleteFile(dirPath);
-        
-      })
-      
+      }
+      // Upload to Nertivia CDN
+      if (upload_cdn === 1) {
 
-    }
-    // Upload to Nertivia CDN
-    if (upload_cdn === 1) {
-      // wait untill the write is finished and then upload.
-      writeStream.once("close", async () => {
         if (cancelRequest) return;
         const success = await nertiviaCDN.uploadFile(fs.createReadStream(dirPath), req.user.uniqueID, fileid, filename)
-          .catch((err:any) => {res.status(403).json({message: err})})
+          .catch((err: any) => { res.status(403).json({ message: err }) })
         if (!success) return deleteFile(dirPath);
-        const fileObj: {url: string, dimensions?: object} = {
+        const fileObj: { url: string, dimensions?: object } = {
           url: `https://media.nertivia.tk/${req.user.uniqueID}/${fileid}/${encodeURIComponent(filename)}`
         };
-        if (metadata){
+        if (metadata) {
           fileObj.dimensions = { width: metadata.width, height: metadata.height };
         }
-        req.uploadFile = {file: fileObj, message}
+        req.uploadFile = { file: fileObj, message }
         next()
         // delete local file after job is done.
         deleteFile(dirPath);
-      })
-    }
-
+      }
+    })
   })
 }
 
-function deleteFile(path:string) {
+function deleteFile(path: string) {
   fs.unlink(path, err => {
     if (err) console.error(err)
   });
 }
 
-function isImage(fileName: string, mimeType: string ) {
+function isImage(fileName: string, mimeType: string) {
   const filetypes = /jpeg|jpg|gif|png/;
   const mimeTypeTest = filetypes.test(mimeType);
   const extname = filetypes.test(path.extname(fileName).toLowerCase());
