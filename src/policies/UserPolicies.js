c3af7f96bcb2ebb2ad695efbf321b626feae1010
supertiger1234 <saawalkhn@googlemail.com>
2020-07-16 09:50:33 +0100

    fixed a bug where you can have empty usernames

--- src/policies/UserPolicies.js
+++ src/policies/UserPolicies.js
@@ -3,7 +3,7 @@ const policyHandler = require('./policyHandler');
 
 const policies = {
   updateUser: [
-    check('username').isString().withMessage('Invalid Format.').isLength({ min: 3, max: 30 }).withMessage("Username must be between 3 - 30 chars long.")
+    check('username').isString().withMessage('Invalid Format.').trim().isLength({ min: 3, max: 30 }).withMessage("Username must be between 3 - 30 chars long.")
       .not().contains(':').withMessage("username cannot contain :").optional({ checkFalsy: true }),
 
     check('tag').isString().withMessage('Invalid Format.').isAlphanumeric().withMessage('Tag must contain alphanumeric characters and no spaces.').isLength({ min: 4, max: 4 }).withMessage('Tag must be 4 characters long.').optional({ checkFalsy: true }),
@@ -14,7 +14,7 @@ const policies = {
     policyHandler
   ],
   updateBot: [
-    check('username').isString().withMessage('Invalid Format.').isLength({ min: 3, max: 30 }).withMessage("Username must be between 3 - 30 chars long.")
+    check('username').isString().withMessage('Invalid Format.').trim().isLength({ min: 3, max: 30 }).withMessage("Username must be between 3 - 30 chars long.")
       .not().contains(':').withMessage("username cannot contain :").optional({ checkFalsy: true }),
     check('tag').isString().withMessage('Invalid Format.').isAlphanumeric().withMessage('Tag must contain alphanumeric characters and no spaces.').isLength({ min: 4, max: 4 }).withMessage('Tag must be 4 characters long.').optional({ checkFalsy: true }),
     check('avatar').isString().withMessage('Invalid Format.').optional({ checkFalsy: true }),
