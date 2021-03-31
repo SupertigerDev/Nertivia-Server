061c0d8ee803f57516ed4446b9507d8302ddc157
Supertiger <saawalkhn@googlemail.com>
2021-03-31 15:38:38 +0100

    fixed cors bug

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -3,14 +3,11 @@ import cors from "cors";
 export default cors({
   origin: function(origin, callback) {
     if (!origin) return callback(null, true);
-    if (
-      JSON.parse(process.env.ALLOWED_ORIGINS).indexOf(origin) === -1 &&
-      !JSON.parse(process.env.ALLOWED_ORIGINS)
-    ) {
-      callback(null, false);
-    } else {
-      callback(null, true);
+    if (JSON.parse(process.env.ALLOWED_ORIGINS).indexOf(origin) === -1) {
+      return callback(null, false)
     }
+    return callback(null, true)
+    
   },
   credentials: true,
 });
