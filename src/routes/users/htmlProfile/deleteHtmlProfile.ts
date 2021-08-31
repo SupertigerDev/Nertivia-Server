f8844e767ba4e09028a08a9a7b319b6cfa5f71a6
Supertiger <saawalkhn@googlemail.com>
2021-08-31 09:56:09 +0100

    added delete html profile route

---
+++ src/routes/users/htmlProfile/deleteHtmlProfile.ts
@@ -0,0 +1,8 @@
+import { NextFunction, Request, Response } from "express-serve-static-core";
+
+import Users from "../../../models/users";
+
+export const deleteHtmlProfile = async (req: Request, res: Response, next: NextFunction) => {
+  await Users.updateOne({_id: req.user._id}, {$unset: {htmlProfile: 1}})
+  res.status(200).json({status: "done"})
+};
--- src/routes/users/htmlProfile/index.ts
+++ src/routes/users/htmlProfile/index.ts
@@ -5,12 +5,16 @@ const authenticate = require("../../../middlewares/authenticate");
 
 import {addHtmlProfile} from './addHtmlProfile';
 import {getHtmlProfile} from './getHtmlProfile';
+import {deleteHtmlProfile} from './deleteHtmlProfile';
 
 htmlProfileRouter.route('/')
   .post(authenticate(true), addHtmlProfile);
 
 htmlProfileRouter.route('/')
   .get(authenticate(), getHtmlProfile);
+
+htmlProfileRouter.route('/')
+  .delete(authenticate(), deleteHtmlProfile);
 
 
 
