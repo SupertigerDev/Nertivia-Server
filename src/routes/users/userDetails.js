f5d08b6eb8b9ad6b1648aac1e21c1f48b2976962
Supertiger <saawalkhn@googlemail.com>
2022-04-09 10:56:26 +0100

    renamed variables

--- src/routes/users/userDetails.js
+++ src/routes/users/userDetails.js
@@ -5,15 +5,13 @@ import {Servers} from "../../models/Servers";
 import {Friends} from "../../models/Friends";
 
 module.exports = async (req, res, next) => {
-  let userID = req.params.user_id;
+  let userId = req.params.user_id;
 
-  if (!userID) {
-    userID = req.user.id;
+  if (!userId) {
+    userId = req.user.id;
   }
 
-  const user = await Users.findOne({
-    id: userID
-  })
+  const user = await Users.findOne({ id: userId })
     .select("-status -__v -friends +about_me +badges +servers +createdBy +htmlProfile")
     .populate('createdBy', 'username tag id -_id')
     .lean();
@@ -25,13 +23,13 @@ module.exports = async (req, res, next) => {
   }
 
 
-  //check if user is blocked.
+  // check if you have blocked the user.
   const isBlocked = await BlockedUsers.exists({
     requester: req.user._id, // blocked by
     recipient: user._id // blocked user
   })
   
-  if (userID === req.user.id) {
+  if (userId === req.user.id) {
     res.json({
       user: {...user, servers: undefined},
       isBlocked
@@ -40,26 +38,26 @@ module.exports = async (req, res, next) => {
   }
 
   // get common servers
-  const requesterServersIDs = ((await Users.findOne({_id: req.user._id}).select("servers").lean()).servers || []).map(s => s.toString());
-  const userServerIDs = (user.servers || []).map(s => s.toString());
+  const requesterServerIds = ((await Users.findOne({_id: req.user._id}).select("servers").lean()).servers || []).map(s => s.toString());
+  const userServerIds = (user.servers || []).map(s => s.toString());
 
-  const commonServers_ID = requesterServersIDs.filter(s => {
-    return userServerIDs.includes(s)
+  const commonServerObjectIds = requesterServerIds.filter(s => {
+    return userServerIds.includes(s)
   })
-  const commonServersID = (await Servers.find({_id: {$in: commonServers_ID}}).select("server_id")).map(s => s.server_id);
+  const commonServerIds = (await Servers.find({_id: {$in: commonServerObjectIds}}).select("server_id")).map(s => s.server_id);
 
   // get common friends
-  const requesterFriendsArr = (await Friends.find({requester: req.user._id, status: 2})).map(f => f.recipient.toString());
-  const userFriendsArr = (await Friends.find({requester: user._id, status: 2})).map(f => f.recipient.toString());
-  const commonFriend_idArr = requesterFriendsArr.filter(r => userFriendsArr.includes(r));
-  const commonFriendID = (await Users.find({_id: {$in: commonFriend_idArr}}).select("id")).map(u => u.id);
+  const requesterFriends = (await Friends.find({requester: req.user._id, status: 2})).map(f => f.recipient.toString());
+  const userFriends = (await Friends.find({requester: user._id, status: 2})).map(f => f.recipient.toString());
+  const commonFriendObjectIds = requesterFriends.filter(r => userFriends.includes(r));
+  const commonFriendIds = (await Users.find({_id: {$in: commonFriendObjectIds}}).select("id")).map(u => u.id);
 
 
 
   res.json({
     user: {...user, servers: undefined},
-    commonServersArr: commonServersID,
-    commonFriendsArr: commonFriendID,
+    commonServersArr: commonServerIds,
+    commonFriendsArr: commonFriendIds,
     isBlocked
   });
 };
